f8d83db86fe361ec6bdfec60ec75889626ef33b2
kyahki <genjikyle123@gmail.com>
2024-04-13 14:14:24 +0800

    ok nani oten

--- legPlan.php
+++ legPlan.php
@@ -11,7 +11,7 @@ $resultset = mysqli_query($connection, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnlyFit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="css/plans.css" rel="stylesheet">
+    <link href="css/plans2.css" rel="stylesheet">
 </head>
 <body>
     <header>OnlyFit</header>
@@ -22,9 +22,7 @@ $resultset = mysqli_query($connection, $sql);
           </a>
           
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
+            
             <li class="nav-item">
               <a class="nav-link" href="register.php#registrationForm">Register</a>
             </li>
@@ -37,24 +35,38 @@ $resultset = mysqli_query($connection, $sql);
             <li class="nav-item">
               <a class="nav-link" href="contactUs.php">Contact Us</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Go Back Home</a>
+            </li>
           </ul>
         </div>
     </nav>
     <br>
+<br>
+<br>
     <br>
-    <div class="about-section">
+<br>
+<br>
+<div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+    <div class="card p-4 custom-container" style="max-width: 900px; font-size: 19px;">
+        <div class="text-center">
         <h1><b>LEG DAY</b></h1>
-        <p>"Leg Day: Strengthen and tone your lower body with a challenging leg workout. Target your quadriceps, hamstrings, glutes, and calves to build a powerful foundation and improve overall athleticism."</p>
-        <p>The exercises in these will usually involve movements targeting your quadriceps, hamstrings, glutes, and calves.</p>
-        <p>Some examples would be:</p>
-        <img src="https://i.pinimg.com/originals/b9/3c/3e/b93c3e936373ec223ef16924ac1e4903.jpg" alt="leg_day_sample_image">
-        
-        <form action="" method="post">
-            <input type="hidden" name="workoutdesciprtion" value="Leg Day: Strengthen and tone your lower body with a challenging leg workout. Target your quadriceps, hamstrings, glutes, and calves to build a powerful foundation and improve overall athleticism. The exercises in these will usually involve movements targeting your quadriceps, hamstrings, glutes, and calves.">
-            <input type="hidden" name="workouttype" value="leg">
-            <button type="submit" name="btnRegister" class="btnPrimaryA">Create Leg Workout Plan</button>
-        </form>
+            <p>"Leg Day: Strengthen and tone your lower body with a challenging leg workout. Target your quadriceps, hamstrings, glutes, and calves to build a powerful foundation and improve overall athleticism."</p>
+            <p>The exercises in these will usually involve movements targeting your quadriceps, hamstrings, glutes, and calves.</p>
+            <p>Some examples would be:</p>
+            <img src="https://i.pinimg.com/originals/b9/3c/3e/b93c3e936373ec223ef16924ac1e4903.jpg" width="75%" height="75%" alt="sample_leg_day">
+            <form action="" method="post">
+                <input type="hidden" name="workoutdesciprtion" value="Leg Day: Strengthen and tone your lower body with a challenging leg workout. Target your quadriceps, hamstrings, glutes, and calves to build a powerful foundation and improve overall athleticism. The exercises in these will usually involve movements targeting your quadriceps, hamstrings, glutes, and calves.">
+                <input type="hidden" name="workouttype" value="leg">
+                <button type="submit" name="btnRegister" class="btnPrimaryA">Create Leg Workout Plan</button>
+            </form>
+        </div>
     </div>
+</div>
+<br>
+<br>
+<br>
+
 
     <footer>
         <p>Peter Sylvan L. Vecina | Kyle T. Vasquez</p>
--- pushPlan.php
+++ pushPlan.php
@@ -11,7 +11,7 @@ $resultset = mysqli_query($connection, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnlyFit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="css/plans.css" rel="stylesheet">
+    <link href="css/plans2.css" rel="stylesheet">
 </head>
 <body>
     <header>OnlyFit</header>
@@ -22,9 +22,6 @@ $resultset = mysqli_query($connection, $sql);
           </a>
           
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="index.php">Home</a>
-            </li>
             <li class="nav-item">
               <a class="nav-link" href="register.php#registrationForm">Register</a>
             </li>
@@ -37,25 +34,41 @@ $resultset = mysqli_query($connection, $sql);
             <li class="nav-item">
               <a class="nav-link" href="contactUs.php">Contact Us</a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Go Back Home</a>
+            </li>
           </ul>
         </div>
     </nav>
     <br>
     <br>
-    <div class="about-section">
-        <h1><b>PUSH DAY</b></h1>
+    <br>
+    
+    <div class="container d-flex justify-content-center align-items-center" style="height: 100vh;">
+        <div class="card p-4 custom-container" style="max-width: 900px; font-size: 19px;">
+            <div class="text-center">
+            <h1><b>PUSH DAY</b></h1>
         <p>"Push Day: It's time to target those upper body muscles with a high-intensity workout focused on pushing movements. Engage your chest, shoulders, and triceps as you press, lift, and push your way to strength and definition."</p>
         <p>The exercises in these will usually involve pushing movements using your chest, triceps, shoulders.</p>
         <p>Some examples would be:</p>
-        <img src="https://i.pinimg.com/736x/fe/93/b2/fe93b2cdefcfc9455acb147067fad962.jpg" alt="samplepush">
+        <img src="https://i.pinimg.com/736x/fe/93/b2/fe93b2cdefcfc9455acb147067fad962.jpg" width="75%" height="75%" alt="samplepush">
         
         <form action="" method="post">
             <input type="hidden" name="workoutdesciprtion" value="Push Day: It's time to target those upper body muscles with a high-intensity workout focused on pushing movements. Engage your chest, shoulders, and triceps as you press, lift, and push your way to strength and definition. The exercises in these will usually involve pushing movements using your chest, triceps, shoulders.">
             <input type="hidden" name="workouttype" value="push">
             <button type="submit" name="btnRegister" class="btnPrimaryA">Create Push Workout Plan</button>
         </form>
-    </div>
+      </div>
 
+      </div>
+
+          </div>
+          <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
     <footer>
         <p>Peter Sylvan L. Vecina | Kyle T. Vasquez</p>
         <p>Bachelor of Computer Science | Year 2</p>
