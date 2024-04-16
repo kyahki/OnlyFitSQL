5deb94e3287642c473995033d0ddc0f87204bade
Peter <xsterfo456@gmail.com>
2024-04-16 20:10:42 +0800

    changed Background

--- customPlan.php
+++ customPlan.php
@@ -11,7 +11,7 @@ $resultset = mysqli_query($connection, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OnlyFit</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="css/customPlanStylee.css" rel="stylesheet">
+    <link href="css/customPlanStyle.css" rel="stylesheet">
     <script src="js/script.js"></script>
 </head>
 <body>
